fbbbfaa3bb01d72e8ef24e2996b12b9d997f0096
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2023-12-26 18:59:13 +0700

    Mengimplementasikan Style Appbar

--- app/src/main/java/com/example/consumerestapi/ui/theme/KontakApp.kt
+++ app/src/main/java/com/example/consumerestapi/ui/theme/KontakApp.kt
@@ -2,8 +2,12 @@ package com.example.consumerestapi.ui.theme
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -70,5 +74,16 @@ fun TopAppBarKontak(
     scrollBehavior: TopAppBarScrollBehavior? = null,
     navigateUp: () -> Unit = {}
 ){
-
+    CenterAlignedTopAppBar(title = { Text(title)},
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack){
+                IconButton(onClick = navigateUp) {
+                    Icon(imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "")
+                }
+            }
+        }
+    )
 }

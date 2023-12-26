f2c727eb9ad673c2f471202eaf9c97b5fb153dc4
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2023-12-26 18:49:58 +0700

    First Commit

---
+++ app/src/main/java/com/example/consumerestapi/ui/home/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.consumerestapi.ui.home.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.consumerestapi.ui.home.HomeViewModel
+import com.example.consumerestapi.KontakApplication
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiMars().container.kontakRepository)
+        }
+    }
+}
+fun CreationExtras.aplikasiMars(): KontakApplication = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as KontakApplication)

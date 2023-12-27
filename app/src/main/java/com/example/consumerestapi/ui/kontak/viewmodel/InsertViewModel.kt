946750cbb19c8bb7c48a9151cf03152a34f685d7
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2023-12-27 14:28:08 +0700

    Membuat function UpdateInsertKontakState

--- app/src/main/java/com/example/consumerestapi/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/kontak/viewmodel/InsertViewModel.kt
@@ -5,9 +5,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.consumerestapi.repository.KontakRepository
+import com.example.consumerestapi.ui.home.viewmodel.InsertUiEvent
 import com.example.consumerestapi.ui.home.viewmodel.InsertUiState
 
 class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
     var insertKontakState by mutableStateOf(InsertUiState())
     private set
+
+    fun updateInsertKontakState(insertUiEvent: InsertUiEvent){
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
 }

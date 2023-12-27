a62f338728ce4aa4294ed5a8c66f688188cff350
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2023-12-27 14:26:06 +0700

    Membuat class InsertViewModel

--- app/src/main/java/com/example/consumerestapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/screen/HomeScreen.kt
@@ -14,24 +14,33 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.consumerestapi.R
 import com.example.consumerestapi.model.Kontak
+import com.example.consumerestapi.navigation.DestinasiNavigasi
+import com.example.consumerestapi.ui.home.HomeViewModel
 import com.example.consumerestapi.ui.home.KontakUIState
+import com.example.consumerestapi.ui.theme.TopAppBarKontak
 
 @Composable
 fun HomeStatus(
@@ -97,7 +106,7 @@ fun KontakLayout(
         items(kontak){kontak ->
             KontakCard(kontak = kontak, modifier = Modifier
                 .fillMaxWidth()
-                .clickable{onDetailClick(kontak)},
+                .clickable { onDetailClick(kontak) },
                 onDeleteClick = {
                     onDeleteClick(kontak)
                 }
@@ -146,3 +155,4 @@ fun KontakCard(
         }
     }
 }
+
---
+++ app/src/main/java/com/example/consumerestapi/ui/kontak/viewmodel/InsertViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.consumerestapi.ui.kontak.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.consumerestapi.repository.KontakRepository
+import com.example.consumerestapi.ui.home.viewmodel.InsertUiState
+
+class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
+    var insertKontakState by mutableStateOf(InsertUiState())
+    private set
+}
